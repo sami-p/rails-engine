08a1473e02cfcf292eb35c462ee6b58d68370fad
sami-p <sami.peterson14@gmail.com>
2021-10-25 14:34:57 -0600

    Create models with validations and relationships

---
+++ app/models/invoice.rb
@@ -0,0 +1,8 @@
+class Invoice < ApplicationRecord
+  has_many :invoice_items, dependent: :destroy
+  has_many :items, through: :invoice_items
+  has_many :transactions
+
+  validates :customer_id, presence: true
+  validates :status, presence: true
+end
