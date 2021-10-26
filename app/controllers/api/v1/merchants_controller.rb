2d125609f1a856d29077ef3a73cd400b4632e2c0
sami-p <sami.peterson14@gmail.com>
2021-10-26 10:13:43 -0600

    Create show action in MerchantsController

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -3,4 +3,13 @@ class Api::V1::MerchantsController < ApplicationController
     merchants = Merchant.by_page(page = params[:page].to_i, per_page = params[:per_page].to_i)
     render json: MerchantSerializer.new(merchants)
   end
+
+  def show
+    if params[:id] && Merchant.exists?(params[:id])
+      merchant = Merchant.find(params[:id])
+      render(json: MerchantSerializer.new(merchant))
+    else
+      render json: { error: 'ERROR: Merchant Not Found.' }, status: :not_found
+    end
+  end
 end
