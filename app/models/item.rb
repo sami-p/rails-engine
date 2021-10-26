3264ac012b5652e183510ce4d1d0713d42a54964
sami-p <sami.peterson14@gmail.com>
2021-10-26 13:58:44 -0600

    Create by page pagination method in item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,10 @@ class Item < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
   validates :unit_price, presence: true
+
+  def self.by_page(page = 1, per_page = 20)
+    page = 1 if page < 1
+    per_page = 20 if per_page < 1
+    limit(per_page).offset((page -1) * per_page)
+  end
 end
