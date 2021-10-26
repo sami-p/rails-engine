8eb6bcf58ca0cc43554a75d78703fc2203fc0064
sami-p <sami.peterson14@gmail.com>
2021-10-26 13:31:29 -0600

    Test get items for merchant

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe "Merchants API" do
-  it "sends a list of merchants" do
+  it "retrieves a list of merchants" do
     create_list(:merchant, 3)
 
     get '/api/v1/merchants'
