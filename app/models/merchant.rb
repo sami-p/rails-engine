45aa56b2df5606da58166c9e24f51ff3566a6951
sami-p <sami.peterson14@gmail.com>
2021-10-26 09:28:33 -0600

    Create by page, pagination method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,10 @@
 class Merchant < ApplicationRecord
   has_many :items, dependent: :destroy
   has_many :invoices
+
+  def self.by_page(page = 1, per_page = 20)
+    page = 1 if page < 1
+    per_page = 20 if per_page < 1
+    limit(per_page).offset((page -1) * per_page)
+  end
 end
