832b84c297b4212701b6035bd1d67c401c757568
sami-p <sami.peterson14@gmail.com>
2021-10-26 13:58:54 -0600

    Add route for item show action

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
       resources :merchants, only: [:index, :show] do
         resources :items, module: 'merchants', only: [:index]
       end
-      resources :items, only: [:index]
+      resources :items, only: [:index, :show]
     end
   end
 end
