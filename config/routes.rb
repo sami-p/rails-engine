98fa41b09f96e8dc75899346e9fba48006659142
sami-p <sami.peterson14@gmail.com>
2021-10-28 16:15:06 -0600

    Add find and find all routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
+      get '/merchants/find', to: 'merchants#find'
+      get '/items/find_all', to: 'items#find_all'
+      
       resources :merchants, only: [:index, :show] do
         resources :items, module: 'merchant', only: [:index]
       end
+
       resources :items, except: [:new] do
-        # resources :merchant, module: 'item', only: [:index]
         get 'merchant', to: 'items/merchant#index'
       end
     end
