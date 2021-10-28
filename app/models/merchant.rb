e20818fd9d33b2d8c47c2b6b8938b63c13c212b3
sami-p <sami.peterson14@gmail.com>
2021-10-28 17:45:14 -0600

    Create top merchants by revenue method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -11,4 +11,15 @@ class Merchant < ApplicationRecord
   def self.search_by_name(name)
     where("name ILIKE ?", "%#{name}%").order("name")
   end
+
+  def self.top_merchants_by_revenue(quantity)
+    joins(invoices: [:invoice_items, :transactions])
+    .select('merchants.*,
+             SUM(invoice_items.quantity * invoice_items.unit_price) AS revenue')
+    .where("transactions.result = 'success'")
+    .group(:id)
+    .order(revenue: :desc)
+    .order('name')
+    .limit(quantity)
+  end
 end
